81c48239c3151316fdb05abbf1594c1415543779
noushin94 <noushin_ahmadvand@yahoo.com>
2023-04-24 09:15:46 +0330

    packages

--- regressors.py
+++ regressors.py
@@ -8,3 +8,12 @@ df.head()
 # allocating x and y
 X = df.drop('median_house_value', axis=1)
 Y = df['median_house_value']
+
+#packages
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
