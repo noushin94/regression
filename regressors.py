c970c8b3a3f7129130d69519e1713b91d1ffec50
noushin94 <noushin_ahmadvand@yahoo.com>
2023-04-24 09:11:04 +0330

    allocating x and y

--- regressors.py
+++ regressors.py
@@ -4,3 +4,7 @@ import numpy as np
 #reading file
 df = pd.read_csv(r'/content/sample_data/california_housing_train.csv')
 df.head()
+
+# allocating x and y
+X = df.drop('median_house_value', axis=1)
+Y = df['median_house_value']
